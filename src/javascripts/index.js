00837e896f551816fb2fdb5ac02aea5749bb4bb7
ben <benjaminmullins@mail.weber.edu>
2020-03-26 19:07:49 -0600

    final commit

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,44 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+import "bootstrap"
+
+const MakeBox = function(rand) {
+    if(rand < .5) {
+        document.write('<div class="red box"></div>')
+    }
+    else {
+        document.write('<div class="blue box"></div>')
+    }
+}
+
+function displayNBoxesUsingFor(i) {
+    for (let i = 0; i < 10; ++i) {
+        MakeBox(Math.random())
+    }
+}
+
+function displayNBoxesUsingWhile(j) {
+    let i = 0
+    while (i < j) {
+        i += 1
+        MakeBox(Math.random())
+    }
+}
+
+function displayNBoxesUsingDo(j) {
+    let i = 0
+    do {
+        i += 1
+        MakeBox(Math.random())
+    } while (i < j)
+}
+
+const square = function(x) {
+    return x * x
+}
+
+displayNBoxesUsingDo(5)
+displayNBoxesUsingFor(10)
+displayNBoxesUsingWhile(15)
+alert(square(21))
